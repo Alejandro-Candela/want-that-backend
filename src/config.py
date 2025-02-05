78b237a03bfe806e1193bb9e4e9fcc8efb98166f
alex-candela <alex.candela@outlook.com>
2025-02-05 22:50:32 +0100

    Add detection thresholds and fix SAM checkpoint path

--- src/config.py
+++ src/config.py
@@ -1,6 +1,11 @@
 import torch
 
 GROUNDING_DINO_MODEL = "IDEA-Research/grounding-dino-base"
-SAM_CHECKPOINT_PATH  = "src\models\sam_vit_b_01ec64.pth"  # Ajusta a la ruta local del checkpoint si es necesario
+SAM_CHECKPOINT_PATH = "src/models/sam_vit_b_01ec64.pth"  # Using forward slashes
 SAM_MODEL_TYPE       = "vit_b"                # "vit_h", "vit_l", "vit_b", etc.
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+BOX_THRESHOLD=0.3
+TEXT_THRESHOLD=0.1
+
+
